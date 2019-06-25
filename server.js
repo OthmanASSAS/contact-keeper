67c40543d098a87e65d3662c56b4d8de0c55efd2
Othman Assas <othman_assas@hotmail.fr>
2019-06-25 23:45:23 +0200

    User model and validation

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const app = express();
 // Connect Database
 connectDB();
 
-app.get('/', (req, res)=> res.json({msg:'Hello world'}))
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.json({ msg: 'Hello world' }))
 
 
 // Define Routes
