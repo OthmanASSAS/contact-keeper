0a17c81c6b8b13ba9fc8461948949184de6dfc9a
Othman Assas <othman_assas@hotmail.fr>
2019-07-02 17:45:34 +0200

    Logout & Navbar full

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -9,13 +9,17 @@ const Navbar = ({ title, icon }) => {
 
     const { isAuthenticated, logout, user } = authContext;
 
+    const onLogout = () => {
+        logout();
+    };
+
     const authLinks = (
         <Fragment>
             <li>Hello {user && user.name}</li>
             <li>
-                <a href="#!">
+                <a onClick={onLogout} href="#!">
                     <i className="fas fa-sign-out-alt"></i>
-                    <span className="hide-sm" onClick={()=>logout()}>Logout</span>
+                    <span className="hide-sm" >Logout</span>
                 </a>
             </li>
         </Fragment>
